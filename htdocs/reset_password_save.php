8b686c05b15247483baf1f136cbeb06b8764b692
SamratDuttaOfficial <41472530+SamratDuttaOfficial@users.noreply.github.com>
2020-07-31 10:47:56 +0530

    Encryption simplified for viewers

--- htdocs/reset_password_save.php
+++ htdocs/reset_password_save.php
@@ -15,18 +15,8 @@ $collegecode = htmlspecialchars($_POST['collegecode']);
 $secret_info = htmlspecialchars($_POST['secret_info']);
 $secret_ans = htmlspecialchars($_POST['secret_answer']);
 $pass = htmlspecialchars($_POST['password']);
-$pass1 = hash('sha384', $pass);
-$pass2 = hash('sha256', $pass1);
-$pass3 = hash('md5', $pass2);
-$pass4 = hash('snefru', $pass3);
-$pass5 = hash('crc32', $pass4);
-$password = hash('sha512', $pass5);
-$secret_ans1 = hash('sha256', $secret_ans);
-$secret_ans2 = hash('sha384', $secret_ans1);
-$secret_ans3 = hash('snefru', $secret_ans2);
-$secret_ans4 = hash('md5', $secret_ans3);
-$secret_ans5 = hash('sha512', $secret_ans4);
-$secret_answer = hash('crc32', $secret_ans5);
+$password = hash('sha256', $pass);
+$secret_answer = hash('sha512', $secret_anss);
  
 
 $query = $conn->query("update members set password = '$password' where gradyear = '$gradyear' and username='$username' and collegecode='$collegecode' and secret_info='$secret_info' and secret_answer='$secret_answer' ");
